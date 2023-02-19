599afa22493731a7ac06d61e0da83a63fd98e2ce
Idrisselouiri <contact.devlper@gmail.com>
2023-02-19 15:21:56 +0100

    phone

---
+++ script.js
@@ -0,0 +1,9 @@
+let img = document.querySelector(".img");
+let container = document.querySelector(".container");
+function changeImage(phone)  { 
+    img.src = phone
+} 
+function changeColor(color){
+    container.style.background = color
+}
+   
